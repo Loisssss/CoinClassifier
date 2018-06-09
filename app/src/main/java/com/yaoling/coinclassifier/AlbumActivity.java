73bbe7f399375ca4d9d301bf63accf84cac07b4e
yaoling <24580>
2018-06-09 14:58:26 +0100

    AlbumActivity load image from album

--- app/src/main/java/com/yaoling/coinclassifier/AlbumActivity.java
+++ app/src/main/java/com/yaoling/coinclassifier/AlbumActivity.java
@@ -1,9 +1,16 @@
 package com.yaoling.coinclassifier;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +20,8 @@ public class AlbumActivity extends AppCompatActivity {
     private ImageView imageView;
     private TextView textView;
     private Button button;
+    private static final int IMAGE = 1;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,5 +31,37 @@ public class AlbumActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textView);
         button = findViewById(R.id.button);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //get photo path
+        if (requestCode == IMAGE && resultCode == AlbumActivity.RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumns = {MediaStore.Images.Media.DATA};
+            Cursor c = getContentResolver().query(selectedImage, filePathColumns, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePathColumns[0]);
+            String imagePath = c.getString(columnIndex);
+            showImage(imagePath);
+            c.close();
+        }
+    }
+
+
+    public void PickImage(View view){
+
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, IMAGE);
+    }
+
+    private void showImage(String imagePath){
+
+        Bitmap bm = BitmapFactory.decodeFile(imagePath);
+        imageView.setImageBitmap(bm);
+
     }
 }
